7477b5a6ae1d1b8b55d671cc3ef1460d9059ab71
saaddalida <dalidassaad@gmail.com>
2019-10-09 16:02:04 -0300

    chore: Routes Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import Routes from './routes';
 import GlobalStyle from './styles/global';
 
 function App() {
-  return <GlobalStyle />;
+  return (
+    <>
+      <GlobalStyle />
+      <Routes />
+    </>
+  );
 }
 
 export default App;
